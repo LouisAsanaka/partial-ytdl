5578f7a4293fb4aabe886308dc25f348fe572df6
Louis Asanaka <25756888+LouisAsanaka@users.noreply.github.com>
2019-11-19 21:49:48 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# partial-ytdl
+
+An easy tool to only download certain parts of a Youtube video. Useful for getting clips from extremely long videos.
+
+Installing
+----------
+- Clone the repository or [download the zip file from GitHub](https://github.com/LouisAsanaka/partial-ytdl/archive/master.zip)
+- Install dependencies:
+    - Download [ffmpeg](https://www.ffmpeg.org/download.html) & add the executable to your PATH
+    - ```pip install -r requirements.txt```
+
+Usage
+-----
+- Run the program with ```python main.py```
---
+++ main.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import youtube_dl
+import subprocess
+import random, string
+from pprint import pprint
+
+def subtract_time(start, end):
+    FMT = '%H:%M:%S'
+    delta = datetime.strptime(end, FMT) - datetime.strptime(start, FMT)
+    return str(delta)
+
+url = input("Enter URL: ")
+start_time = input("Enter start time (in seconds, or in hh:mm:ss[.xxx] form): ")
+end_time = input("Enter end time (in seconds, or in hh:mm:ss[.xxx] form): ")
+duration = subtract_time(start_time, end_time)
+
+ffmpeg_command = "ffmpeg -hide_banner"
+
+with youtube_dl.YoutubeDL({}) as ydl:
+    info_dict = ydl.extract_info(url, download=False)
+    title = info_dict['title']
+    formats = info_dict['requested_formats']
+    for format in formats:
+        format_url = format['url']
+        ffmpeg_command += f' -ss "{start_time}" -i "{format_url}"'
+    random_name = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    ffmpeg_command += f' -t "{duration}" -c copy -map 0:v -map 1:a -c:v libx264 -c:a aac "{random_name}-{title}.mp4"'
+    subprocess.run(ffmpeg_command)
+subprocess.run("cls", shell=True)
+print("Done!")
+input()
